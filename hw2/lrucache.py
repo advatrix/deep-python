c04145c07bdfe87689b91497602943d27e08bb31
advatrix <trojan.advatrix@gmail.com>
2020-04-04 20:17:35 +0300

    lrucache

--- hw2/lrucache.py
+++ hw2/lrucache.py
@@ -4,43 +4,45 @@ from collections import OrderedDict
 class ICache:
     def __init__(self, capacity: int=10) -> None:
         # self._heap = [0] * capacity # last times used
-		self._storage = OrderedDict()
-		self._capacity = capacity
-		
+        self._storage = OrderedDict()
+        self._capacity = capacity
+
     def get(self, key: str) -> str:
-		if key in self._storage:
-			ret = self._storage[key]
-			self._storage.move_to_end(key)
-			return ret
-		else:
-			return None
+        if key in self._storage:
+            ret = self._storage[key]
+            self._storage.move_to_end(key)
+            return ret
+        else:
+            return ''
+        
+    def delete(self, key: str) -> None:
+            if key in self._storage:
+                del self._storage[key]
 
     def set(self, key: str, value: str) -> None:
-        if len(self._storage) == self._capacity:
-			self._storage.popitem(last=False)
-		self._storage[key] = value
+        if len(self._storage) == self._capacity and key not in self._storage.keys():
+            self._storage.popitem(last=False)
+        self._storage[key] = value
 
-    def del(self, key: str) -> None:
-        if key in self._storage:
-			del self._storage[key]
-    
-	def __rrp
-		
-'''	
-class Cashable:
-	
-	def __init__(self, key: str, value: str):
-		self._time = time.time()
-		self._key = key
-		self._value = value
-		
-	def update(self):
-		self._time = time.time()
-		'''
+    def __repr__(self):
+        return repr(self._storage)
 
 
 if __name__ == '__main__':
-	c = ICache()
-	c.set(12, 34)
-	c.set(45, 66)
-	
+    c = ICache(3)
+    print(repr(c))
+    c.set(12, 34)
+    print(repr(c))
+    c.set(45, 66)
+    print(repr(c))
+    print(c.get(12))
+    print(repr(c))
+    c.set(16, 32)
+    print(c)
+    c.set(19, 21)
+    print(c)
+    print(c.get(45))
+    c.delete(55)
+    print(c)
+    c.delete(16)
+    print(c)
